253bbad6576b7c6fc41173fcf567f8b6ed41effd
Faygo44 <faigao44@gmail.com>
2013-04-16 23:28:01 -0500

    Update project.java

--- project.java
+++ project.java
@@ -1,84 +1,27 @@
 import java.io.*;
-import java.nio.charset.Charset;
 
 import opennlp.tools.postag.*;
-import opennlp.tools.sentdetect.*;
-import opennlp.tools.util.*;
-import opennlp.tools.util.model.ModelType;
 
 public class project {
 
-  private static void getFile(String inputFileName, String outputFileName) throws IOException
+  private static void getFile(File folder, File output)
 	{
-
-
-
+		for (final File fileEntry : folder.listFiles()) {
+		    if (fileEntry.isDirectory()) {
+		        getFile(fileEntry, output);
+		    } else {
+		        System.out.println(fileEntry.getName());
+		    }
+		}
+	  
+		String temp = "Test one tests test two.";
+		String array[] = temp.split(" ");
 	} // end getFile...
-
-	
-	/*
-	 * Below was getting to know the parser.
-	 * train() will not actually be used in this code.
-	 * It is more for reference to understand our code.
-	 */
-  /*
-	private static void train ()
-	{
-
-		POSModel model = null;
-
-		InputStream dataIn = null;
-		try {
-			dataIn = new FileInputStream("en-pos.train");
-			ObjectStream<String> lineStream = new PlainTextByLineStream(dataIn, "UTF-8");
-			ObjectStream<POSSample> sampleStream = new WordTagSampleStream(lineStream);
-
-			model = POSTaggerME.train("en", sampleStream, TrainingParameters.defaultParams(), null, null);
-		}
-		catch (IOException e) {
-			// Failed to read or parse training data, training failed
-			e.printStackTrace();
-		}
-		finally {
-			if (dataIn != null) {
-				try {
-					dataIn.close();
-				}
-				catch (IOException e) {
-					// Not an issue, training already finished.
-					// The exception should be logged and investigated
-					// if part of a production system.
-					e.printStackTrace();
-				}
-			}
-		}
-
-		OutputStream modelOut = null;
-		try {
-			modelOut = new BufferedOutputStream(new FileOutputStream(modelFile));
-			model.serialize(modelOut);
-		}
-		catch (IOException e) {
-			// Failed to save model
-			e.printStackTrace();
-		}
-		finally {
-			if (modelOut != null) {
-				try {
-					modelOut.close();
-				}
-				catch (IOException e) {
-					// Failed to correctly save model.
-					// Written model might be invalid.
-					e.printStackTrace();
-				}
-			}
-		}
-
-	} // end train...
-	*/
-
-	private static void POSTag (){
+  
+  	/*
+  	 * Parse a sentence array and show the tags to each word
+  	 */
+	private static void POSTag (String sent[], File output){
 		InputStream modelIn = null;
 
 		try {
@@ -86,8 +29,8 @@ public class project {
 		  POSModel model = new POSModel(modelIn);
 			
 			POSTaggerME tagger = new POSTaggerME(model);
-			String sent[] = new String[]{"Most", "large", "cities", "in", "the", "US", "had",
-	                "morning", "and", "afternoon", "newspapers", "."};		  
+			//String sent[] = new String[]{"Most", "large", "cities", "in", "the", "US", "had",
+	                //"morning", "and", "afternoon", "newspapers", "."};
 			String tags[] = tagger.tag(sent);
 			
 			for(int i=0; i<tags.length; i++)
@@ -107,12 +50,18 @@ public class project {
 		  }
 		}			
 	}
-
-
+	
+	/*
+	 * Below was getting to know the parser.
+	 * train() will not actually be used in this code.
+	 * It is more for reference to understand our code.
+	 */
+	private static void train ()
 
 	public static void main (String args[])
 	{
-		POSTag();
+		File folder = new File(args[0]);
+		getFile(folder, args[1];
 	} // end main...
 
 }
