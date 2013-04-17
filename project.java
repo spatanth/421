4ea5ff0d2cc6e585e30e495e249a3efaf5ff27c0
Faygo44 <faigao44@gmail.com>
2013-04-16 23:33:46 -0500

    Update project.java
    Fixed a couple minor errors

--- project.java
+++ project.java
@@ -11,17 +11,18 @@ public class project {
 		        getFile(fileEntry, output);
 		    } else {
 		        System.out.println(fileEntry.getName());
+		  	  
+				String temp = "Test one tests test two.";
+				String array[] = temp.split(" ");
+				POSTag(array, output);
 		    }
 		}
-	  
-		String temp = "Test one tests test two.";
-		String array[] = temp.split(" ");
 	} // end getFile...
   
   	/*
   	 * Parse a sentence array and show the tags to each word
   	 */
-	private static void POSTag (String sent[], File output){
+	private static void POSTag(String sent[], File output){
 		InputStream modelIn = null;
 
 		try {
@@ -56,12 +57,68 @@ public class project {
 	 * train() will not actually be used in this code.
 	 * It is more for reference to understand our code.
 	 */
+  /*
 	private static void train ()
+	{
+
+		POSModel model = null;
+
+		InputStream dataIn = null;
+		try {
+			dataIn = new FileInputStream("en-pos.train");
+			ObjectStream<String> lineStream = new PlainTextByLineStream(dataIn, "UTF-8");
+			ObjectStream<POSSample> sampleStream = new WordTagSampleStream(lineStream);
+
+			model = POSTaggerME.train("en", sampleStream, TrainingParameters.defaultParams(), null, null);
+		}
+		catch (IOException e) {
+			// Failed to read or parse training data, training failed
+			e.printStackTrace();
+		}
+		finally {
+			if (dataIn != null) {
+				try {
+					dataIn.close();
+				}
+				catch (IOException e) {
+					// Not an issue, training already finished.
+					// The exception should be logged and investigated
+					// if part of a production system.
+					e.printStackTrace();
+				}
+			}
+		}
+
+		OutputStream modelOut = null;
+		try {
+			modelOut = new BufferedOutputStream(new FileOutputStream(modelFile));
+			model.serialize(modelOut);
+		}
+		catch (IOException e) {
+			// Failed to save model
+			e.printStackTrace();
+		}
+		finally {
+			if (modelOut != null) {
+				try {
+					modelOut.close();
+				}
+				catch (IOException e) {
+					// Failed to correctly save model.
+					// Written model might be invalid.
+					e.printStackTrace();
+				}
+			}
+		}
+
+	} // end train...
+	*/
 
 	public static void main (String args[])
 	{
 		File folder = new File(args[0]);
-		getFile(folder, args[1];
+		File output = new File(args[1]);
+		getFile(folder, output);
 	} // end main...
 
 }
