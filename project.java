fbf30fbfd39025140b61d253d69aa6178d96609a
spatanth <anthony.spatafora@gmail.com>
2013-04-18 23:40:30 -0500

    Update project.java

--- project.java
+++ project.java
@@ -23,29 +23,51 @@ import opennlp.tools.util.model.ModelType;
 
 public class project {
 
-	//Grades the file by going through each sentence and comparing tags
 	private static void grader(File output) throws FileNotFoundException
 	{
+		// scans file and sets each tag to a spot in the String array
 		Scanner scanner = new Scanner(output);
 		String[] arr = null;
-		int temp = 0;
+		int arrSize = 0;
 		
 		while ( scanner.hasNext() ) {
-			arr[temp] = scanner.next();
-			temp++;
+			arr[arrSize] = scanner.next();
+			arrSize++;
 		}
 		scanner.close();
 		
-		while (temp != 0) {
+		// calls grading and looks at each individual tag in the array
+		int temp = 0;
+		char tense = ' ';
+		char retTense = ' ';
+		while (temp != arrSize) {
 			
-			grading(arr[temp]);
-			temp--;
+			retTense = grading(arr[temp]);
+			
+			if ( (tense != ' ') && (tense != retTense) )
+				System.out.println("Tense error: " + retTense + "!");
+			
+			// increment along the array
+			temp++;
 		}
 	}
 	
-	//Grades the functions based on tense and possession
-	private static void grading(String x)
+	/* This method uses a switch case statement to determine the tense of
+	 * tag that was sent to it. Here is a quick key to show what the switch-
+	 * case statement is doing.
+	 * 
+	 * Key:
+	 * Singular Noun = n
+	 * Plural Noun = N
+	 * Singular Proper Noun = p
+	 * Plural Proper Noun = P
+	 * Verb Past Tese | Verb Past Participle = v
+	 * Verb Gerund | Present Participle | Non-3rd Person Singular Present | Non-3rd Person Singular Present
+	 * 
+	 */
+	private static char grading(String x)
 	{
+		char currTense = ' ';
 		
 		switch (x) {
 		case "CC": // Coordinating conjunction
@@ -71,12 +93,16 @@ public class project {
 		case "MD": // Modal
 			break;
 		case "NN": // Noun, singular or mass
+			currTense = 'n';
 			break;
 		case "NNS": // noun, plural
+			currTense = 'N';
 			break;
 		case "NNP": // Proper noun, singular
+			currTense = 'p';
 			break;
 		case "NNPS": // Proper noun, plural
+			currTense = 'P';
 			break;
 		case "PDT": // Predeterminer
 			break;
@@ -103,14 +129,19 @@ public class project {
 		case "VB": // Verb, base form
 			break;
 		case "VBD": // Verb, past tense
+			currTense = 'v';
 			break;
 		case "VBG": // Verb, gerund or present participle
+			currTense = 'V';
 			break;
 		case "VBN": // Verb, past participle
+			currTense = 'v';
 			break;
 		case "VBP": // Verb, non-3rd person singular present
+			currTense = 'V';
 			break;
 		case "VBZ": // Verb, 3rd person singular present
+			currTense = 'V';
 			break;
 		case "WDT": // Wh-determiner
 			break;
@@ -120,14 +151,14 @@ public class project {
 			break;
 		case "WRB": // Wh-adverb
 			break;
+
+
+		} // end switch...
 		
-		
-		}
-		
-	}
+		return currTense;
+	} // end grading...
 	
-	//Function to read a file, or read through a folder and tag each of the files
-	private static void getFile(File folder, FileWriter output) throws FileNotFoundException
+	private static void getFile(File folder, FileWriter output) throws IOException
 	{
 		for (final File fileEntry : folder.listFiles()) {
 		    if (fileEntry.isDirectory()) {	//If the given input is a folder, search through the folder
@@ -143,14 +174,13 @@ public class project {
 				for(int i=0; i<array.length; i++){
 					System.out.println(array[i]);
 				}
-				//Calls the following functions to tag and grade the current file
 				POSTag(array, output);
 				countSentences(array, output);
 		    }
 		}
 		
 	} // end getFile...
-
+	
 	/*
 	 * Parse a sentence array and show the tags to each word
 	 */
@@ -158,12 +188,10 @@ public class project {
 		InputStream modelIn = null;
 
 		try {
-			//File required to use the tagger
 			modelIn = new FileInputStream("en-pos-maxent.bin");
 			POSModel model = new POSModel(modelIn);
 
 			POSTaggerME tagger = new POSTaggerME(model);
-			//Tag the current file
 			String tags[] = tagger.tag(sent);
 
 			for(int i=0; i<tags.length; i++)
@@ -184,8 +212,8 @@ public class project {
 			}
 		}			
 	} // end POSTag...
-  
-  	//Count the number of sentences in the given file
+
+  	// Count the number of sentences in the given file
   	private static void countSentences(String sent[], FileWriter output) throws IOException{
 		int sentences = 0;
 		float score = 0;
@@ -220,8 +248,8 @@ public class project {
 		}
 		System.out.println(sentences);
 		System.out.println(score);
-  	}//End countSentences
-
+  	} // end countSentences...
+	
 	public static void main (String args[]) throws IOException
 	{
 		File folder = new File(args[0]);	//The given path of the folder of files
@@ -231,11 +259,11 @@ public class project {
 	} // end main...
 
 
-	/*
+	/****************************************************
 	 * Below was getting to know the parser.
 	 * train() will not actually be used in this code.
 	 * It is more for reference to understand our code.
-	 */
+	 ****************************************************/
 	/*
 	private static void train ()
 	{
