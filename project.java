ed9c65a2b5b1cd6b3729361cfeb18e42ff1c6c95
Faygo44 <faigao44@gmail.com>
2013-05-06 19:08:04 -0500

    Update project.java

--- project.java
+++ project.java
@@ -21,7 +21,8 @@ import java.util.*;
 import opennlp.tools.postag.*;
 
 public class project {
-	static float scores[][];
+	static int scores[][];
+	static String finalGrade[][];
 	private static File[] files;
 	private static int totalFiles;
 	private static int currentFile = 0;
@@ -177,7 +178,6 @@ public class project {
 		        System.out.println(fileEntry.getName());
 		    	@SuppressWarnings("resource")
 				String content = new Scanner(new File(fileEntry.getAbsolutePath())).useDelimiter("\\Z").next();
-		    	//System.out.println(content + '\n');
 		        //Splits the scanned file into individual words
 				String array[] = content.split("[.\n\r\n]");
 				POSTag(array);
@@ -196,17 +196,42 @@ public class project {
 		try {
 			modelIn = new FileInputStream("en-pos-maxent.bin");
 			POSModel model = new POSModel(modelIn);
-
+			
+			String fileWords[] = new String [1000];
+			String fileTags[] = new String[1000];
+			int current = 0;
+			String pronouns[] = new String[100];
+			int temp1 = 0;
+			
 			POSTaggerME tagger = new POSTaggerME(model);
 			for(int x=0; x<sent.length; x++){
-				System.out.println(sent[x]);
 				String array[] = sent[x].split(" ");
 				String tags[] = tagger.tag(array);
 	
-				for(int i=0; i<tags.length; i++)
-					System.out.print(tags[i] + ' ');
-				System.out.println();
+				//Used in part 2
+				for(int i=0; i<tags.length; i++){
+					if(array[i].length() > 1){
+						fileWords[current] = array[i];
+						fileTags[current] = tags[i];
+						current++;
+						
+						if(tags[i].equals("PRP") || tags[i].equals("PRP$")){	//Get the pronouns in the file
+							pronouns[temp1] = array[i].toLowerCase();
+							temp1++;
+						}
+					}
+				}
 			}
+			
+			for(int i=0; i<current; i++)
+				System.out.print(fileWords[i] + '\t');
+			System.out.println();
+			for(int i=0; i<current; i++)
+				System.out.print(fileTags[i] + '\t');
+			System.out.println();
+			
+			checkPerson(pronouns);
+			checkTopic(fileWords, fileTags);
 		}
 		catch (IOException e) {
 			// Model loading failed, handle the error
@@ -223,61 +248,158 @@ public class project {
 		}			
 	} // end POSTag...
 
-  	// Count the number of sentences in the given file
+	/*
+	 * Count the number of sentences in the given file
+	 * Split by a newline, carriage return, or a period
+	 * Discount any that are only one word long.
+	 */
   	private static void countSentences(String sent[]) throws IOException{
 		float sentences = 0;
-		float score = 0;
+		float curr = 0;
+		int score;
 		for(int i=0; i<sent.length; i++){
 			String temp[] = sent[i].split("[ \n]");
 			if(temp.length > 1)	//If the sentence length is more than one word it may be a proper sentence
 				sentences++;
 		}
-		score = sentences / 6;
+		curr = sentences / 6;
 		//Calculate the scores of each of the files rounded down to the nearest half
-		if(score > 1){
+		if(curr > 1){
 			score = 5;
 			scores[6][currentFile] = score;
 		}else{
-			score = score * 5;
-			double comp = score - Math.floor(score);
-			if(comp >= 0.75){	//If the score is closer to the next number, round it to the higher number
-				score = (float) (Math.floor(score) + 1.0);
-				if(score > 5) score = 5;	//If the score if more than 5, round it down to 5
-				scores[6][currentFile] = score;
-			}else if(comp >= 0.25){	//If the score is closer to the midpoint of two numbers, round it to the nearest half
-				score = (float) (Math.floor(score) + 1.0);
-				score += 0.5;
-				scores[6][currentFile] = score;
-			}else{ //If the score is closer to the lower number, round it down
-				score = (float) (Math.floor(score));
-				scores[6][currentFile] = score;
-			}
+			scores[6][currentFile] = (int)(Math.floor(curr * 5));
 		}
-		System.out.println(sentences);
-		System.out.println(score);
   	} // end countSentences...
+  	
+  	/*
+  	 * Currently only checks if the pronouns are first, second, or third person
+  	 * and grades with how many their are
+  	 */
+  	private static void checkPerson(String pronouns[]){
+  		Set<String> first = new HashSet<String>(Arrays.asList(new String[] {"i", "i'm", "me", "my", "mine", "we", "us", "our"}));
+  		Set<String> second = new HashSet<String>(Arrays.asList(new String[] {"you", "your"}));
+  		Set<String> third = new HashSet<String>(Arrays.asList(new String[] {"he", "she", "him", "her", "his", "hers", "they", "them", "their", "theirs", "it"}));
+  		
+  		int score = 5;
+  		
+  		//for(int x=0; pronouns[x] != null; x++)
+  			//System.out.print(pronouns[x] + '\t');
+  		//System.out.println();
+  		for(int x=0; pronouns[x] != null; x++){
+  			if(first.contains(pronouns[x])) {
+  				//System.out.print("one" + '\t');
+  			} else if(second.contains(pronouns[x])) {
+  				score -= 1;
+  				//System.out.print("two" + '\t');
+  			} else if(third.contains(pronouns[x])) {
+  				score += 1;
+  				//System.out.print("three" + '\t');
+  			} else {
+  				//System.out.print('\t');
+  			}
+  		}
+  		if(score > 5)
+  			score = 5;
+  		else if (score < 0)
+  			score = 0;
+  		
+  		scores[4][currentFile] = score;
+  		//System.out.println();
+  	}
+  	
+  	private static void checkTopic(String array[], String tags[]){
+  		String nouns[] = new String[100];
+  		int temp = 0;
+  		int total = 0;
+  		int properNouns = 0;
+  		
+  		for(int i=0; array[i] != null; i++){
+	  		if(tags[i].equals("NN") || tags[i].equals("NNS")) {	//Get the nouns in the file
+				nouns[temp] = array[i].toLowerCase();
+				temp++;
+				total++;
+			} else if(tags[i].equals("NNP") && !tags[i-1].equals("NNP")) {	//If it is a proper noun, add it to the list
+				properNouns++;
+				total++;
+			}
+  		}
+  		
+  		//Come From, live, family, work, school
+  		Set<String> family = new HashSet<String>(Arrays.asList(new String[] 
+  				{"family", "child", "children", "kid", "kids", "son", "daughter",
+  				"parent", "father", "mother", "sibling", "brother", "sister"}));
+  		Set<String> workSchool = new HashSet<String>(Arrays.asList(new String[]
+  				{"work", "job", "school", "education", "learn", "educate"}));
+  		
+  		int contained = properNouns;
+  		for(int x=0; nouns[x] != null; x++) {
+  			if(family.contains(nouns[x]) || workSchool.contains(nouns[x]))
+  				contained++;
+  			System.out.print(nouns[x] + ' ');
+  		}
+  		System.out.println("\n" + contained + ' ' + total);
+  		
+  		float score = (float)contained / (float)total;
+  		System.out.println(score);
+  		if(score >= 0.4)
+  			scores[5][currentFile] = 5;
+  		else if(score >= 0.3)
+  			scores[5][currentFile] = 4;
+  		else if(score >= 0.2)
+  			scores[5][currentFile] = 3;
+  		else if(score >= 0.1)
+  			scores[5][currentFile] = 2;
+  		else if(score > 0)
+  			scores[5][currentFile] = 1;
+  		else
+  			scores[5][currentFile] = 0;
+  	}
   	
   	//Prints the scores of each of the files into the output file, and to the screen
   	private static void printScores(FileWriter output) throws IOException{
+  		System.out.println("\nEssay\t1a\t1b\t1c\t1d\t2a\t2b\t3a\tFinal Grade");
   		for(int y=0; y<totalFiles; y++){
+  			finalGrade(y);
   			System.out.print(files[y].getName() + "\t");
   			output.write(files[y].getName() + "\t");
   			for(int x=0; x<7; x++){
   				System.out.print(scores[x][y] + "\t");
   				output.write(scores[x][y] + "\t");
   			}
+  			System.out.print(finalGrade[0][y]);
+  			output.write(finalGrade[0][y]);
   			System.out.println();
   			output.write("\r\n");
   		}
   	}
   	
+  	private static void finalGrade(int current){  		
+		float total = scores[0][current] + scores[1][current] + scores[2][current] + (2 * scores[3][current]) + scores[4][current] + ( 3 * scores[5][current]) + scores[6][current];
+		total = total / 10;
+		double comp = total - Math.floor(total);
+		
+		if(comp >= 0.75) {	//If the total is closer to the next number, round it to the higher number
+			total = (float) (Math.floor(total) + 1.0);
+			if(total > 5) total = 5;	//If the total if more than 5, round it down to 5
+		} else if(comp >= 0.25) {	//If the total is closer to the midpoint of two numbers, round it to the nearest half
+			total = (float) (Math.floor(total) + 0.5);
+		} else {	//If the total is closer to the lower number, round it down
+			total = (float) (Math.floor(total));
+			if(total > 5) total = 5;
+		}
+		
+		finalGrade[0][current] = Float.toString(total);
+  	}
+  	
   	//Initializes a 2d array of size 7x(number of files) to 0
   	private static void initializeScores(int length){
-  		scores = new float[7][length];
-  		for(int x=0; x<7; x++){
-  			for(int y=0; y<length; y++){
+  		scores = new int[7][length];
+  		finalGrade = new String[1][length];
+  		for(int y=0; y<length; y++){
+  			for(int x=0; x<7; x++)
   				scores[x][y] = 0;
-  			}
+  			finalGrade[0][y] = "";
   		}
   	}
 	
@@ -285,7 +407,7 @@ public class project {
 	{
 		File folder = new File(args[0]);	//The given path of the folder of files
 		FileWriter output = new FileWriter(args[1]);	//The output file to print the tags
-		output.write("Essay\t1a\t1b\t1c\t1d\t2a\t2b\t3a\r\n"); //Prints a header to the file
+		output.write("Essay\t1a\t1b\t1c\t1d\t2a\t2b\t3a\tFinal Grade\r\n"); //Prints a header to the file
     	files = folder.listFiles();
     	totalFiles = files.length;
     	initializeScores(totalFiles);
@@ -300,6 +422,7 @@ public class project {
 	 * train() will not actually be used in this code.
 	 * It is more for reference to understand our code.
 	 ****************************************************/
+	
 	/*
 	private static void train ()
 	{
